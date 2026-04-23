cf58f04c191f65b441284a7dee923bb0f7ab5c1a
20261ctb0100052-coder <20261ctb0100052@estudantes.ifpr.edu.br>
2026-04-23 19:21:34 +0000

    first commit

---
+++ beecrowd/1000-hello-world.py
@@ -0,0 +1,15 @@
+'''
+Problema: beecrowd | 1000
+Data: 2011.fevereiro.18
+Estudante: Pedro Lucas
+'''
+# Objetivo: Exibir exatamente a mensagem "Hello Word!" na tela
+
+# ---ANÁLISE (LIAC) ---
+# Entrada: nenhuma - este problrma não requer leitura de dados
+# Processamento: nenhum - não há cálculo ou transformação
+# Saída: exibir o texto "Hello Word!" exatamente como especificado
+
+# print() é o comando de saída em Python
+# Equivalente ao bloco "diga ..." do Scratch
+print('Hello Word!')
